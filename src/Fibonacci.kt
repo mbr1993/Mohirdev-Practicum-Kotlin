5e802d8a8ef2d0f4cfaba01fd29ac0261de758b4
mbr1993 <boburmannonov93@gmail.com>
2022-03-23 12:50:24 +0500

    fibonacci

--- src/Fibonacci.kt
+++ src/Fibonacci.kt
@@ -1,7 +1,8 @@
 fun main() {
-
+    for (i in 1..10) println(fibonacci(i))
 }
 
-fun fibonacci(number: Int) {
-    return
+fun fibonacci(number: Int): Int {
+    if (number <= 1) return 1
+    return fibonacci(number - 1) + fibonacci(number - 2)  //recursive function
 }
