2523554ee0f33b84fd82cdbd89c345da328e4dc6
mbr1993 <boburmannonov93@gmail.com>
2022-03-23 23:23:45 +0500

    secondary constructor

--- src/Class.kt
+++ src/Class.kt
@@ -1,16 +1,17 @@
 fun main() {
-    val myCar = Car(brand = "Ford", model = "Mustang", year = 1969)
-    val myCar2 = Car("Nexia", "Chevrolet", 2015)
+    val myCar = Car("Nexia", "Chevrolet", 2015)
 
     println("I have a ${myCar.brand} ${myCar.model} from ${myCar.year}")
-    println("${myCar2.model}  ${myCar2.brand}  ${myCar2.year}")
-
-    myCar.drive()
-    myCar.stop()
 }
 
-class Car(val brand: String, val model: String, val year: Int) {
+class Car {
+    val brand: String
+    val model: String
+    val year: Int
 
-    fun drive() = println("Driving")
-    fun stop() = println("Stopping")
+    constructor(brand: String, model: String, year: Int) {
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
 }
